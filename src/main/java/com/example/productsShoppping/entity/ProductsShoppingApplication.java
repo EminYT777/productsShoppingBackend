509d64206c322be1bab5f893e801651b8468503c
EminYT777 <your_email@example.com>
2025-11-01 14:56:05 +0400

    add jwt security

--- src/main/java/com/example/productsShoppping/entity/ProductsShoppingApplication.java
+++ src/main/java/com/example/productsShoppping/entity/ProductsShoppingApplication.java
@@ -1,4 +1,4 @@
-package com.example.productsShoppping;
+package com.example.productsShoppping.entity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/example/productsShoppping/entity/User.java
+++
@@ -1,4 +0,0 @@
-package com.example.productsShoppping.entity;
-
-public class User {
-}
